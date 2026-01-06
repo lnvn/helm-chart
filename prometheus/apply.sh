2047cfb2d31d1bf942c6bcaa3842125b4c8a20aa
Long Nguyen <faker2407@gmail.com>
2026-01-06 12:44:10 +0700

    update apply script & enable minikube ingress

--- prometheus/apply.sh
+++ prometheus/apply.sh
@@ -1,1 +1,12 @@
-helm upgrade -i prometheus oci://ghcr.io/prometheus-community/charts/kube-prometheus-stack -n monitoring --values values.yaml
+NS=monitoring
+
+if kubectl get namespace "$NS" >/dev/null 2>&1; then
+  echo "Namespace '$NS' already exists. Skipping..."
+else
+  echo "Namespace '$NS' not found. Creating it..."
+  kubectl create namespace "$NS"
+fi
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+
+helm upgrade -i prometheus oci://ghcr.io/prometheus-community/charts/kube-prometheus-stack -n monitoring --values values.yaml --version 80.10.0
